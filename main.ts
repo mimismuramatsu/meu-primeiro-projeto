e4bfa11acb4de5637127fa8006a4ef0045cfb701
mimismuramatsu <0000111287835xsp@al.educacao.sp.gov.br>
2025-03-26 09:23:25 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Giraffe)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Heart)
